85f04324a63ad02ee903c09958e7a46f1fcd7e29
guestSoftMe <73720335+guestSoftMe@users.noreply.github.com>
2020-11-24 21:49:17 +0200

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  $('.contentBasket_rating').addRating();
+  $('.contentBasketL_rating').addRating();
+  $('.contentBasketR_rating').addRating();
+  $('.contentBasketT_rating').addRating();
+  $('.contentBasketB_rating').addRating();
+  $('.contentBasketTotal_rating').addRating();
+  $('.contentBasketProduct_ratingCR').addRating();
+  $('.contentBasketProduct_ratingCL').addRating();
+  $('.contentBasketProduct_ratingCT').addRating();
+  $('.contentBasketProduct_ratingCB').addRating();
+  $('.mens_rowBodyBasketRatingL').addRating();
+  $('.mens_rowBodyBasketRatingR').addRating();
+  $('.mens_rowBodyBasketRatingB').addRating();
+  $('.mens_rowBodyBasketRatingT').addRating();
+  $('.mens_rowBodyBasketRatingP').addRating();
+  $('.mens_rowBodyBasketRatingK').addRating();
+});
